d928e374eed543a009606f6cacf012ac75413165
Rowan <rowanolive12@gmail.com>
2023-05-01 18:51:34 +1200

    Updated Contact form, more bootstrap elements

--- src/Contact.js
+++ src/Contact.js
@@ -1,18 +1,57 @@
 import './Contact.css';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function Contact() {
     return (<div className="Section" id="Contact">
-        <div className="Contactbox">
-            <h1 className="Title"> Contact me </h1>
-        <form action="" method="get">
-            <div className="formitems">
-                    <label for="name">Name: </label>
-                    <input type="text" name="name" id="name" required />
-                    <label for="email">Email: </label>
-                    <input type="text" name="email" id="email" required />
-                    <label for="message">Message: </label>
-                    <input type="text" name="message" id="message" required />
-            </div>
-            </form>
+      <div className="Contactbox">
+        <Container fluid>
+          <Row>
+            <h1 className="contactTitle"> Contact me </h1>
+          </Row>
+          <Row>
+            <Form.Label htmlFor="inputName">Name :</Form.Label>
+            <Form.Control
+              type="text"
+              id="inputName"
+              aria-describedby="nameHelp"
+              placeholder="Your Name"
+            />
+            <Form.Text id="nameHelp" muted>
+              Insert your name here.
+            </Form.Text>
+          </Row>
+          <Row>
+            <Form.Label htmlFor="inputEmail">Email :</Form.Label>
+            <Form.Control
+              type="text"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              placeholder="name@example.com"
+            />
+            <Form.Text id="emailHelp" muted>
+              Insert your email here.
+            </Form.Text>
+          </Row>
+          <Row>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="inputMessage" >Message :</Form.Label>
+              <Form.Control
+                type="text"
+                id="inputMessage"
+                aria-describedby="messageHelp"
+                as="textarea"
+                rows={4}
+                placeholder="Your Message"
+              />
+              <Form.Text id="messageHelp" muted>
+                Insert the message you want to send.
+              </Form.Text>
+            </Form.Group>
+          </Row>
+        </Container>
         </div>
     </div>);
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import './Profile.css'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Profile() {
     return (
         <div className="Section" id="About me">
         <div className="Profilebox">
-            <img className="Profilepicture" src="https://kitcat.com.sg/wp-content/uploads/2020/07/Kit-Cat.png" alt="Rowan Olive" />
-            <div>
-            <h1 id="Title">About me</h1>
-                    <p>My name is Rowan and I am a junior software developer.
-                        I mainly have experience in C#, Javascript (React).
-                        I also have experience with backend such as SQL and Jade.
-                        I have experience in deploying full stack cloud applications.</p>
-            </div>
+          <Container fluid>
+            <Row >
+              <Col xs={3} md={2 }>
+                <div className="Profilepicture">
+                  <Image src="https://kitcat.com.sg/wp-content/uploads/2020/07/Kit-Cat.png" alt="Rowan Olive" fluid />
+                </div>
+              </Col>
+                <Col>
+                <h1 className="Title" id="Title">About me</h1>
+                <p className="profiletext">My name is Rowan and I am a junior software developer.
+                  I mainly have experience in C#, Javascript (React).
+                  I also have experience with backend such as SQL and Jade.
+                    I have experience in deploying full stack cloud applications.</p>
+                  </Col>
+            </Row>
+          </Container>
             </div >
         </div>
     )
